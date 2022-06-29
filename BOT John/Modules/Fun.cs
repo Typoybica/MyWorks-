39544421d2ca546d8a497af2dde2d08abb600f79
Demidenko Vladislav <71293273+Typoybica@users.noreply.github.com>
2022-06-29 18:07:17 +0300

    Add files via upload

---
+++ BOT John/Modules/Fun.cs
@@ -0,0 +1,38 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Discord;
+
+namespace BOT_John.Modules
+{
+    public class Fun : ModuleBase
+    {
+        [Command("meme")]
+        [Alias("reddit")]
+        public async Task Meme(string subreddit = null)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync($"https://reddit.com/r/{subreddit ?? "memes"}/random.json?limit=1");
+            if (!result.StartsWith("["))
+            {
+                await Context.Channel.SendMessageAsync("This subreddit doesn't exist!");
+                return;
+            }
+            JArray arr = JArray.Parse(result);
+            JObject post = JObject.Parse(arr[0]["data"]["children"][0]["data"].ToString());
+
+            var builder = new EmbedBuilder()
+                .WithImageUrl(post["url"].ToString())
+                .WithColor(new Color(33, 176, 252))
+                .WithTitle(post["title"].ToString())
+                .WithUrl("https://reddit.com" + post["permalink"].ToString())
+                .WithFooter($"📩 {post["num_comments"]} ⬆️ {post["ups"]}");
+            var embed = builder.Build();
+            await Context.Channel.SendMessageAsync(null, false, embed);
+        }
+    }
+}
---
+++ BOT John/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace BOT_John
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+    }
+}
